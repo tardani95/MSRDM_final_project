be2e1e7c58bcf8bc5dbbbef01e808d7d575225df
Daniel Tar <tardani95@gmail.com>
2021-02-25 11:13:57 +0100

    transformation calculation and regressor fix

--- src/matlab_analyse_robot/iterative_euler_lagrange_ur10_6dof.m
+++ src/matlab_analyse_robot/iterative_euler_lagrange_ur10_6dof.m
@@ -51,7 +51,6 @@ Ii(:, :, 4) = I4;
 Ii(:, :, 5) = I5;
 Ii(:, :, 6) = I6;
 
-
 qi = [q1, q2, q3, q4, q5, q6];
 qip = [q1p, q2p, q3p, q4p, q5p, q6p];
 
@@ -74,8 +73,41 @@ relDH_com = [
 Xef_0 = X_link_abs_stack(:, :, end);
 Xef_0_str = char(simplify(Xef_0));
 
-% % compute the transformations regarding to the world coordinate frame
+% compute the transformations regarding to the world coordinate frame
 T0_W = rotm_trvec2tf(RotX(spi / 2), [0; 0; 0]);
+
+Tef_W = T0_W * Ti_0_stack(:, :, end);
+
+Xef_W = tf2pose(Tef_W);
+Xef_W_char = char(simplify(expand(Xef_W)));
+
+T1_0 = Ti_0_stack(:, :, 1);
+T2_0 = Ti_0_stack(:, :, 2);
+T3_0 = Ti_0_stack(:, :, 3);
+T4_0 = Ti_0_stack(:, :, 4);
+T5_0 = Ti_0_stack(:, :, 5);
+T6_0 = Ti_0_stack(:, :, 6);
+
+Tcm1_0 = Tcmi_0_stack(:, :, 1);
+Tcm2_0 = Tcmi_0_stack(:, :, 2);
+Tcm3_0 = Tcmi_0_stack(:, :, 3);
+Tcm4_0 = Tcmi_0_stack(:, :, 4);
+Tcm5_0 = Tcmi_0_stack(:, :, 5);
+Tcm6_0 = Tcmi_0_stack(:, :, 6);
+
+T1_0_char = char(simplify(expand(T1_0)));
+T2_0_char = char(simplify(expand(T2_0)));
+T3_0_char = char(simplify(expand(T3_0)));
+T4_0_char = char(simplify(expand(T4_0)));
+T5_0_char = char(simplify(expand(T5_0)));
+T6_0_char = char(simplify(expand(T6_0)));
+
+Tcm1_0_char = char(simplify(expand(Tcm1_0)));
+Tcm2_0_char = char(simplify(expand(Tcm2_0)));
+Tcm3_0_char = char(simplify(expand(Tcm3_0)));
+Tcm4_0_char = char(simplify(expand(Tcm4_0)));
+Tcm5_0_char = char(simplify(expand(Tcm5_0)));
+Tcm6_0_char = char(simplify(expand(Tcm6_0)));
 
 %% Differential Kinematics
 [jacobi_abs_stack, Ji_0_stack, Jcmi_0_stack] = DiffKinematics(H_abs_stack, 'rrrrrr');
@@ -185,122 +217,101 @@ M_char = char(simplify(expand(M)));
 C_char = char(simplify(expand(C)));
 G_char = char(simplify(expand(G)));
 
-        Tef_W = T0_W * Ti_0_stack(:, :, end);
+%% Robot regressor
+Qrp = sym('q%drp', [length(qi) 1], 'real');
+Qrpp = sym('q%drpp', [length(qi) 1], 'real');
 
-        Xef_W = tf2pose(Tef_W);
-        Xef_W_char = char(simplify(expand(Xef_W)));
+state_vars = [Q' Qp' Qrp' Qrpp' g g_axis'];
 
-        T1_0 = Ti_0_stack(:, :, 1);
-        T2_0 = Ti_0_stack(:, :, 2);
-        T3_0 = Ti_0_stack(:, :, 3);
+eqn = M * Qrpp + C * Qrp + G;
 
-        Tcm1_0 = Tcmi_0_stack(:, :, 1);
-        Tcm2_0 = Tcmi_0_stack(:, :, 2);
-        Tcm3_0 = Tcmi_0_stack(:, :, 3);
+[Yr, Theta] = RobotRegressor(eqn, state_vars);
 
-        T1_0_char = char(simplify(expand(T1_0)));
-        T2_0_char = char(simplify(expand(T2_0)));
-        T3_0_char = char(simplify(expand(T3_0)));
+if sum(expand(eqn - Yr * Theta), 'all') == 0
+    disp("Robot regressor correctly calculated");
+end
 
-        Tcm1_0_char = char(simplify(expand(Tcm1_0)));
-        Tcm2_0_char = char(simplify(expand(Tcm2_0)));
-        Tcm3_0_char = char(simplify(expand(Tcm3_0)));
+Theta_str = char(Theta);
+Yr_str = char(Yr);
 
-        %% Robot regressor
-        Qrp = sym('q%drp', [3 1], 'real');
-        Qrpp = sym('q%drpp', [3 1], 'real');
+disp('Done!');
 
-        state_vars = [Q' Qp' Qrp' Qrpp' g g_axis'];
+% % Relative Homogeneous Transformations
+% T1_0 = H_link_rel_stack(:,:,1);
+% T2_1 = H_link_rel_stack(:,:,2);
+% T3_2 = H_link_rel_stack(:,:,3);
 
-        eqn = M * Qrpp + C * Qrp + G;
+% Tcm1_0 = H_com_rel_stack(:,:,1);
+% Tcm2_1 = H_com_rel_stack(:,:,2);
+% Tcm3_2 = H_com_rel_stack(:,:,3);
 
-        [Yr, Theta] = RobotRegressor(eqn, state_vars);
+% % Homogeneous Transformations wrt WORLD frame
+% % start with indexing 2 because the first element is the frame zero
+% T0_0=H_link_abs_stack(:,:,1);
+% T1_0=H_link_abs_stack(:,:,2);
+% T2_0=H_link_abs_stack(:,:,3);
+% T3_0=H_link_abs_stack(:,:,4);
 
-        if sum(expand(eqn - Yr * Theta), 'all') == 0
-            disp("Robot regressor correctly calculated");
-        end
+% Tcm1_0 = H_com_abs_stack(:,:,1);
+% Tcm2_0 = H_com_abs_stack(:,:,2);
+% Tcm3_0 = H_com_abs_stack(:,:,3);
 
-        Theta_str = char(Theta);
-        Yr_str = char(Yr);
+% % Compute the POSE and jacobian of end-effector with respect to the world
+% % coordinate frame
+% Xef_0 = simplify(HT2PoseVec(T3_0));
+% Jef_0 = jacobian(Xef_0', [q1 q2 q3]);
 
-        disp('Done!');
+% T3_W = H0_w * T3_0;
+% Xef_W = simplify(HT2PoseVec(T3_W));
+% Jef_W = jacobian(Xef_W', [q1 q2 q3]);
 
-        % % Relative Homogeneous Transformations
-        % T1_0 = H_link_rel_stack(:,:,1);
-        % T2_1 = H_link_rel_stack(:,:,2);
-        % T3_2 = H_link_rel_stack(:,:,3);
+% % Compute the POSE and jacobian of the COMs with respect to the world
+% % coordinate frame
+% Xcm1_0 = simplify(HT2PoseVec(Tcm1_0));
+% Xcm2_0 = simplify(HT2PoseVec2(Tcm2_0));
+% Xcm3_0 = simplify(HT2PoseVec2(Tcm3_0));
 
-        % Tcm1_0 = H_com_rel_stack(:,:,1);
-        % Tcm2_1 = H_com_rel_stack(:,:,2);
-        % Tcm3_2 = H_com_rel_stack(:,:,3);
+% xcm1_0 = Xcm1_0(1:3);
+% xcm2_0 = Xcm2_0(1:3);
+% xcm3_0 = Xcm3_0(1:3);
 
-        % % Homogeneous Transformations wrt WORLD frame
-        % % start with indexing 2 because the first element is the frame zero
-        % T0_0=H_link_abs_stack(:,:,1);
-        % T1_0=H_link_abs_stack(:,:,2);
-        % T2_0=H_link_abs_stack(:,:,3);
-        % T3_0=H_link_abs_stack(:,:,4);
+% Jcm1_0 = simplify(jacobian(Xcm1_0', [q1 q2 q3]));
+% Jcm2_0 = simplify(jacobian(Xcm2_0', [q1 q2 q3]));
+% Jcm3_0 = simplify(jacobian(Xcm3_0', [q1 q2 q3]));
 
-        % Tcm1_0 = H_com_abs_stack(:,:,1);
-        % Tcm2_0 = H_com_abs_stack(:,:,2);
-        % Tcm3_0 = H_com_abs_stack(:,:,3);
+% %% converting to string
+% Xef_0_str = char(simplify(Xef_0))
+% Jef_0_str = char(simplify(Jef_0))
 
-        % % Compute the POSE and jacobian of end-effector with respect to the world
-        % % coordinate frame
-        % Xef_0 = simplify(HT2PoseVec(T3_0));
-        % Jef_0 = jacobian(Xef_0', [q1 q2 q3]);
+% Xef_W_str = char(simplify(Xef_W))
+% Jef_W_str = char(simplify(Jef_W))
 
-        % T3_W = H0_w * T3_0;
-        % Xef_W = simplify(HT2PoseVec(T3_W));
-        % Jef_W = jacobian(Xef_W', [q1 q2 q3]);
+% Xcm1_0_str = char(Xcm1_0)
+% Xcm2_0_str = char(Xcm2_0)
+% Xcm3_0_str = char(Xcm3_0)
 
-        % % Compute the POSE and jacobian of the COMs with respect to the world
-        % % coordinate frame
-        % Xcm1_0 = simplify(HT2PoseVec(Tcm1_0));
-        % Xcm2_0 = simplify(HT2PoseVec2(Tcm2_0));
-        % Xcm3_0 = simplify(HT2PoseVec2(Tcm3_0));
+% Jcm1_0_str = char(simplify(Jcm1_0))
+% Jcm2_0_str = char(simplify(Jcm2_0))
+% Jcm3_0_str = char(simplify(Jcm3_0))
 
-        % xcm1_0 = Xcm1_0(1:3);
-        % xcm2_0 = Xcm2_0(1:3);
-        % xcm3_0 = Xcm3_0(1:3);
+% T1_0_str = char(simplify(T1_0))
+% T2_1_str = char(simplify(T2_1))
+% T3_2_str = char(simplify(T3_2))
 
-        % Jcm1_0 = simplify(jacobian(Xcm1_0', [q1 q2 q3]));
-        % Jcm2_0 = simplify(jacobian(Xcm2_0', [q1 q2 q3]));
-        % Jcm3_0 = simplify(jacobian(Xcm3_0', [q1 q2 q3]));
+% Tcm1_0_str = char(simplify(Tcm1_0))
+% Tcm2_1_str = char(simplify(Tcm2_1))
+% Tcm3_2_str = char(simplify(Tcm3_2))
 
-        % %% converting to string
-        % Xef_0_str = char(simplify(Xef_0))
-        % Jef_0_str = char(simplify(Jef_0))
+% Tcm1_0_str = char(simplify(Tcm1_0))
+% Tcm2_0_str = char(simplify(Tcm2_0))
+% Tcm3_0_str = char(simplify(Tcm3_0))
 
-        % Xef_W_str = char(simplify(Xef_W))
-        % Jef_W_str = char(simplify(Jef_W))
-
-        % Xcm1_0_str = char(Xcm1_0)
-        % Xcm2_0_str = char(Xcm2_0)
-        % Xcm3_0_str = char(Xcm3_0)
-
-        % Jcm1_0_str = char(simplify(Jcm1_0))
-        % Jcm2_0_str = char(simplify(Jcm2_0))
-        % Jcm3_0_str = char(simplify(Jcm3_0))
-
-        % T1_0_str = char(simplify(T1_0))
-        % T2_1_str = char(simplify(T2_1))
-        % T3_2_str = char(simplify(T3_2))
-
-        % Tcm1_0_str = char(simplify(Tcm1_0))
-        % Tcm2_1_str = char(simplify(Tcm2_1))
-        % Tcm3_2_str = char(simplify(Tcm3_2))
-
-        % Tcm1_0_str = char(simplify(Tcm1_0))
-        % Tcm2_0_str = char(simplify(Tcm2_0))
-        % Tcm3_0_str = char(simplify(Tcm3_0))
-
-        % T0_W_str = char(H_link_world_stack(:,:,1))
-        % T1_W_str = char(H_link_world_stack(:,:,2))
-        % T2_W_str = char(H_link_world_stack(:,:,3))
-        % T3_W_str = char(H_link_world_stack(:,:,4))
-        %
-        %
-        % Tcm1_W_str = char(H_com_world_stack(:,:,1))
-        % Tcm2_W_str = char(H_com_world_stack(:,:,2))
-        % Tcm3_W_str = char(H_com_world_stack(:,:,3))
+% T0_W_str = char(H_link_world_stack(:,:,1))
+% T1_W_str = char(H_link_world_stack(:,:,2))
+% T2_W_str = char(H_link_world_stack(:,:,3))
+% T3_W_str = char(H_link_world_stack(:,:,4))
+%
+%
+% Tcm1_W_str = char(H_com_world_stack(:,:,1))
+% Tcm2_W_str = char(H_com_world_stack(:,:,2))
+% Tcm3_W_str = char(H_com_world_stack(:,:,3))
